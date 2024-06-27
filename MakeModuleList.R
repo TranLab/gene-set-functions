029c969bae7659d3bae560c025c40a658fd5529d
Tuan M. Tran <46120099+tmtran420@users.noreply.github.com>
2024-06-27 10:00:58 -0400

    Update MakeModuleList.R
    update MakeModuleList comments

--- MakeModuleList.R
+++ MakeModuleList.R
@@ -1,7 +1,7 @@
 # mat = matrix of normalized RNA-seq expression data where each row is a gene and each column is a cluster/cell-type.
 # Cluster data derived from single cells can be median gene expression across all cells within the cluster or sum counts subsampled for a fixed number of cells (e.g. 1000 cells).
-# sd_cutoff = number of standard deviations above the mean expression across all clusters/celltypes for which you want to specify a gene as being included in a module.
-# min_genes = minimum genes that are needed to be in a module for a module to be defined
+# sd_cutoff = Number of standard deviations above the mean expression across all cell types/clusters for determining whether a gene will be included in a module.
+# min_genes = Minimum genes that are needed to be in a module for a module to be defined. Default set at 10.
 
 MakeModList <- function(mat, sd_cutoff = 2, min_genes = 10){
   ScaledMatrix <- scale(t(mat))
